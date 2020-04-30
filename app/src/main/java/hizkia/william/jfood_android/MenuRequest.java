bf44e92d1beeed1b7231c257fd2718eb8d74283f
hizkiawilliam <williamhizkia@gmail.com>
2020-04-30 15:30:13 +0700

    CS Modul 9

---
+++ app/src/main/java/hizkia/william/jfood_android/MenuRequest.java
@@ -0,0 +1,23 @@
+package hizkia.william.jfood_android;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MenuRequest extends StringRequest {
+
+    private static String URL = "http://192.168.1.105:8080/food";
+    private Map<String, String> params;
+
+    public MenuRequest(Response.Listener<String> listener) {
+        super(Method.GET, URL, listener, null);
+        params = new HashMap<>();
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
